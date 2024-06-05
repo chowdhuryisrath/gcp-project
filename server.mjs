7b630499ad4c2db814e6ef8e658d2993129ce12a
chowdhuryisrath <israthchowdhury19@gmail.com>
2024-06-05 15:58:16 -0500

    first commit

---
+++ server.mjs
@@ -0,0 +1,35 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+const port = 3001;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+// Middleware to enable CORS
+app.use(cors());
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // Allow only this origin
+    optionsSuccessStatus: 200 // Some legacy browsers choke on 204
+  };
+  app.use(cors(corsOptions));
+
+// POST endpoint to handle form submission
+app.post('/login', (req, res) => {
+  console.log(req.body)
+  const {  email, password } = req.body;
+
+  console.log('Form data received:', { email, password });
+
+  // Here, you can process the data (e.g., save it to a database, send an email, etc.)
+  
+  // For now, we just send a response back to the client
+  res.status(200).json({ message: 'Form submitted successfully!' });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
